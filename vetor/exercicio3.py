8e72e41945aa81df40a6b4628199621bf2aaa98c
Daniel Rodrigues Da Silva <danicojleite@hotmail.com>
2025-05-25 18:07:42 -0400

    Exercício 3 - vetor

---
+++ vetor/exercicio3.py
@@ -0,0 +1,18 @@
+
+numeros = [0] * 100
+
+for i in range(100):
+    numeros[i] = int(input(f"Digite o número {i+1}: "))
+
+x = int(input("Digite o valor de x: "))
+
+existe = False
+for i in range(100):
+    if numeros[i] == x:
+        existe = True
+        break
+
+if existe:
+    print(f"O valor {x} existe no conjunto.")
+else:
+    print(f"O valor {x} não foi encontrado no conjunto.")
